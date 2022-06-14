6dd3f54c1c65afcc749dd74582a956a5300a70b3
Abduz Zami <69592754+AbduzZami@users.noreply.github.com>
2022-06-14 20:30:46 +0600

    up

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.2
+## 0.0.3
 
 * Support for all 6 platforms.
--- lib/open_street_map_search_and_pick.dart
+++ lib/open_street_map_search_and_pick.dart
@@ -1,7 +1,4 @@
 import 'dart:convert';
-import 'dart:math';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
@@ -9,8 +6,6 @@ import 'package:http/http.dart' as http;
 import 'package:open_street_map_search_and_pick/models/latlong.dart';
 import 'package:open_street_map_search_and_pick/widgets/autocomplete.dart';
 import 'package:open_street_map_search_and_pick/widgets/wide_button.dart';
-
-import 'models/osmdata.dart';
 import 'models/pickeddata.dart';
 
 class OpenStreetMapSearchAndPick extends StatefulWidget {
